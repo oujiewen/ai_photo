9e18156ad7ebcd9531cca55ca1046163ff0eb438
oujiewen <45540534+oujiewen@users.noreply.github.com>
2018-12-05 00:20:46 +0800

    解决后台添加数据失败，增加后台筛选

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -4,5 +4,17 @@ from models import Guest
 from models import Event
 # Register your models here.
 
-admin.site.register(Guest)
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display=['name','status','start_time','id']
+    search_fields=['name','id']
+    list_filter=['status']
+
+
+class GusetAdmin(admin.ModelAdmin):
+    list_display = ['event','realname', 'phone', 'email', 'sign','create_time']
+    search_fields = ['realname', 'sign']
+    list_filter = ['sign']
+
+
+admin.site.register(Guest,GusetAdmin)
+admin.site.register(Event,EventAdmin)
